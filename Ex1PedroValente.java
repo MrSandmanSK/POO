3d6c3ab30d4e58d9b8d8b02556b87f60a6bb8fad
Pedro Valente <44214986+MrSandmanSK@users.noreply.github.com>
2020-08-26 17:41:59 -0300

    Add files via upload

---
+++ Ex1PedroValente.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+// Nome: Pedro Ouverney Sepulveda Valente
+// Matricula: 0050015561
+public class Ex1PedroValente {
+	public static void main(String[] args) {
+		int num = 0;
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.print("Digite um numero entre 0 e 9: ");
+		num = teclado.nextInt();
+		
+		if (num >= 0 && num <=9) {
+			for (int i = 1; i <= 10; i++) {
+				System.out.println(num + " x " + i + " = " + num*i);
+			}
+		}
+		else System.out.println("O numero deve ser entre 0 e 9!");
+	}
+}
